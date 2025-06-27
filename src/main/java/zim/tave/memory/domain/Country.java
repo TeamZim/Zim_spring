e4a214d6eb87145927939765f314246d0f60fa33
jimin <parkjm012@sogang.ac.kr>
2025-06-27 19:31:59 +0900

    fix: Country setter 추가

--- src/main/java/zim/tave/memory/domain/Country.java
+++ src/main/java/zim/tave/memory/domain/Country.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Country {
 
