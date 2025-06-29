dbe1c6617e23ea29062f41f3e8020df569fbb906
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-06-30 00:13:00 +0900

    fix: MyPageService birth format 수정

--- src/main/java/zim/tave/memory/dto/JoinRequestDto.java
+++ src/main/java/zim/tave/memory/dto/JoinRequestDto.java
@@ -1,5 +1,6 @@
 package zim.tave.memory.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,12 +12,25 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class JoinRequestDto {
 
+    @Schema(description = "카카오 사용자 ID(필수)", example = "4317757086")
     private String kakaoId;
+
+    @Schema(description = "프로필 이미지 URL", example = "http://img1.kakaocdn.net/profile.jpeg")
     private String profileImageUrl;
+
+    @Schema(description = "사용자 성", example = "KANG")
     private String surName;
+
+    @Schema(description = "사용자 이름", example = "JIHYE")
     private String firstName;
+
+    @Schema(description = "사용자 한글 이름", example = "강지혜")
     private String koreanName;
+
+    @Schema(description = "생년월일 (yyyy-MM-dd)", example = "2000-01-01")
     private LocalDate birth;
+
+    @Schema(description = "국가명", example = "REPUBLIC OF KOREA")
     private String nationality;
 
 }
--- src/main/java/zim/tave/memory/service/MyPageService.java
+++ src/main/java/zim/tave/memory/service/MyPageService.java
@@ -9,6 +9,7 @@ import zim.tave.memory.repository.DiaryRepository;
 import zim.tave.memory.repository.UserRepository;
 import zim.tave.memory.repository.VisitedCountryRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,9 +22,13 @@ public class MyPageService {
     private final VisitedCountryRepository visitedCountryRepository;
 
     public MyPageResponseDto getMyPage(Long userId) {
+
+
         //유저 조회
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
+
+        String formattedBirth = birthFormatter(user.getBirth());
 
         // UserInfo
         MyPageResponseDto.UserInfo userInfo = new MyPageResponseDto.UserInfo(
@@ -32,7 +37,7 @@ public class MyPageService {
                 user.getSurName(),
                 user.getFirstName(),
                 user.getKoreanName(),
-                user.getBirth(),
+                formattedBirth,
                 user.getNationality()
         );
 
@@ -50,11 +55,28 @@ public class MyPageService {
 
         return new MyPageResponseDto(userInfo, statistics, flags);
     }
+
     private String countryCodeToEmoji(String countryCode) {
         // ISO Alpha-2 코드 기반 이모지 변환
         return countryCode.toUpperCase()
                 .chars()
                 .mapToObj(c -> String.valueOf((char)(c + 127397)))
                 .collect(Collectors.joining());
+    }
+
+    private String birthFormatter(LocalDate birth) {
+        int day = birth.getDayOfMonth();
+        int year = birth.getYear();
+
+        String[] englishMonthForm = {"Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        String[] koreanMonthForm = {"1월", "2월", "3월", "4월", "5월", "6월",
+                "7월", "8월", "9월", "10월", "11월", "12월"};
+
+        int monthIndex = birth.getMonthValue() - 1;
+        String koreanMonth = koreanMonthForm[monthIndex];
+        String englishMonth = englishMonthForm[monthIndex];
+
+        return String.format("%d %s/%s %d", day, koreanMonth, englishMonth, year);
     }
 }
