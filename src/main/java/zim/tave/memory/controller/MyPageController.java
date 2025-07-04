f8256ed67a54c1ff0cece4d8d3abe5ce4c0dbce7
최형원 <choehyungwon@choehyeong-won-ui-MacBookAir.local>
2025-07-04 17:10:52 +0900

    feat: Setting, MyPage swagger 작성

--- src/main/java/zim/tave/memory/controller/MyPageController.java
+++ src/main/java/zim/tave/memory/controller/MyPageController.java
@@ -1,24 +1,41 @@
 package zim.tave.memory.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import zim.tave.memory.dto.LoginResponseDto;
 import zim.tave.memory.dto.MyPageResponseDto;
 import zim.tave.memory.service.MyPageService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/mypage")
+@Tag(name = "MyPage-Controller", description = "마이페이지 조회")
 public class MyPageController {
 
     private final MyPageService myPageService;
 
-    //마이페이지 조회
+    @Operation (summary = "마이페이지 조회",
+        description = "사용자 아이디 이용하여 마이페이지 조회")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "로그인 성공",
+                content = @Content(schema = @Schema(implementation = MyPageResponseDto.class))),
+        @ApiResponse(responseCode = "500", description = "서버 오류, 존재하지 않는 사용자 조회", content = @Content())
+    })
     @GetMapping("/{userId}")
-    public ResponseEntity<MyPageResponseDto> getMyPage(@PathVariable Long userId) {
+    public ResponseEntity<MyPageResponseDto> getMyPage(
+            @Parameter(description = "조회할 사용자 ID", example = "1")
+            @PathVariable Long userId) {
         MyPageResponseDto myPageResponseDto = myPageService.getMyPage(userId);
         return ResponseEntity.ok(myPageResponseDto);
     }
--- src/main/java/zim/tave/memory/controller/SettingController.java
+++ src/main/java/zim/tave/memory/controller/SettingController.java
@@ -1,5 +1,10 @@
 package zim.tave.memory.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,16 +13,27 @@ import zim.tave.memory.service.SettingService;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/setting")
+@Tag(name = "Setting-controller", description = "설정 조회(로그아웃, 회원 탈퇴)")
 public class SettingController {
 
     private final SettingService settingService;
 
+    @Operation(summary = "로그아웃", description = "사용자 로그아웃 처리")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "로그아웃 성공"),
+            @ApiResponse(responseCode = "500", description = "서버 오류, 존재하지 않는 사용자 등", content = @Content())
+    })
     @PatchMapping("/{userId}/logout")
     public ResponseEntity logout(@PathVariable Long userId) {
         settingService.logout(userId);
         return ResponseEntity.ok("로그아웃 완료");
     }
 
+    @Operation(summary = "회원탈퇴", description = "사용자 계정을 삭제")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원탈퇴 성공"),
+            @ApiResponse(responseCode = "500", description = "서버 오류, 존재하지 않는 사용자 등", content = @Content())
+    })
     @DeleteMapping("/{userId}")
     public ResponseEntity delete(@PathVariable Long userId) {
         settingService.deleteAccount(userId);
